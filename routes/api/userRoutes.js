b101a26c2ceb7d3299df87029ce957bd2304f610
Kemiru <pjurkin@gmail.com>
2024-03-20 14:10:27 -0500

    Add user routes and controller functions

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../../controllers/userController');
+
+router.get('/', userController.getAllUsers);
+router.get('/:userId', userController.getUserById);
+router.post('/', userController.createUser);
+router.put('/:userId', userController.updateUser);
+router.delete('/:userId', userController.deleteUser);
+
+router.post('/:userId/friends/:friendId', userController.addFriend);
+router.delete('/:userId/friends/:friendId', userController.removeFriend);
+
+module.exports = router;
