ee63bbfb0ed4e1b000f8b0d8233887aa9e037b79
Kemiru <pjurkin@gmail.com>
2024-03-20 14:09:46 -0500

    Add user model with username, email, thoughts, and friends fields

---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, unique: true, required: true, trim: true },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: /^\S+@\S+\.\S+$/,
+    },
+    thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Thought" }],
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
